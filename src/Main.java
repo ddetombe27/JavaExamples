7a1cf2ad3058285dbeedf0c9d98084691e6e88cc
ddetombe27 <124434344+ddetombe27@users.noreply.github.com>
2025-09-02 12:15:12 +0000

    did dogyear program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main (String[] args) {
-        System.out.println("The Lord of the Rings is not a trilogy");
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int currentYear;
+        int birthYear;
+        int dogBirthYear;
+
+        (1 - 1 == 0) {
+            System.out.println("Math is working");
+        }
+
+        System.out.println("What is the current year?");
+
+        Scanner scan = new Scanner(System.in);
+        currentYear = scan.nextInt();
+        System.out.println("What is your birth year?");
+        birthYear = scan.nextInt();
+        System.out.println("What is your dog's birth year?");
+        dogBirthYear = scan.nextInt();
+
+        boolean isTrilogy = scan.nextBoolean();
+        scan.close();
+
+        System.out.println("You are " + (currentYear - birthYear) + " years old.");
+        System.out.println("Your dog is " + (currentYear - dogBirthYear) + " years old.");
+        System.out.println("Your dog is " + ((currentYear - dogBirthYear) * 7) + " dog years old.");
+
+
+
+
     }
 }
+
+
