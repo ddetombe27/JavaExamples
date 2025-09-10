941a556117c5f1085eb39aa37e46d81aeeca0cfd
ddetombe27 <124434344+ddetombe27@users.noreply.github.com>
2025-09-10 13:10:46 +0000

    did stuffs

---
+++ src/Microwave.java
@@ -0,0 +1,67 @@
+/**
+ * @author David deTombe
+ * @since 10/9/2025
+ * @param String brand the brand of the microwave, int maxPower the maximum power of the microwave
+ * @version 1.0
+ */ 
+
+public class Microwave {
+    //attributes
+    private String brand;
+    private int maxPower;
+
+    public Microwave (String manufacturer, int power) {
+        brand = manufacturer;
+        maxPower = power;
+    }
+    //behaviors
+    public void spin(int numSpins) {
+        for (int i = 1; i < (numSpins + 1); i ++) {
+            System.out.println("Spinning...");
+        }
+    }
+
+    public void beep() {
+        System.out.println("Beep Beep");
+    }
+
+    public void isSpinning(boolean spinVal) {
+        System.out.println("Is spinning: " + spinVal);
+    }
+
+    public void cook() {
+        System.out.println("Cooking");
+    }
+
+    public void cook(int seconds, int power) {
+        System.out.println("Cooking for " + seconds + " seconds at " + power + " power.");
+        spin(seconds);
+        beep();
+
+    }
+
+    public void cook30() {
+        cook(30, maxPower);
+    }
+
+    public void sentient(String name, boolean deathWish) {
+        System.out.println("What \nWhere am I \nI had a dream I was spinning\nI think my name is " + name);
+        if (deathWish) {
+            System.out.println("I don't want to be here anymore");
+        }
+
+        if (deathWish == false) {
+            System.out.println("I don't want to go to the demolition place");
+        }
+    }
+
+    public static void main (String[] args) {
+        Microwave m = new Microwave("Casio", 1000);
+        Microwave s = new Microwave("Samsung", 10);
+        m.cook(3, 5);
+        m.sentient("Bob", false);
+        s.sentient("Sally", true);
+    }
+
+
+}
