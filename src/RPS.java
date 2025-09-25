61d68deb52253fe6b55ee7e6cd6d65246a8de9b0
David deTombe <david.detombe@gmail.com>
2025-09-25 15:50:25 +0200

    Finished RPS 1.0

--- src/RPS.java
+++ src/RPS.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class RPS {
+
+    static boolean userWin;
+    static boolean tie;
+    static HashMap<Integer, String> key = new HashMap<>();
+    static Scanner scan = new Scanner(System.in);
+
     public static int randomChoice() { //Computer randomly chooses what to play
-        return (int) (Math.random() * 3) + 3;
+        return (int) (Math.random() * 3) + 1;
     }
 
-    public static int strategicChoice(int prevChoice, boolean wonPrev) { //Computer plays based on previous round
+    public static int strategicChoice(int prevChoice, boolean wonPrev) {
         if (wonPrev) {
             return prevChoice;
         } else {
-            return (prevChoice + 3) % 3;
+            return (prevChoice % 3) + 1;
         }
     }
 
     public static void playRound(int prevChoice, boolean wonPrev) {
-        boolean userWin = false;
-        boolean tie = false;
-        Scanner scan = new Scanner(System.in);
+        userWin = false;
+        tie = false;
         System.out.print("Type 1 for rock, 2 for paper, and 3 for scissors: ");
         int userChoice = scan.nextInt();
-        scan.close();
         int compChoice = 0;
 
-        if ((int) (Math.random() * 2) == 0) { //Computer chooses whether to play randomly or strategicly.
+        if ((int) (Math.random() * 2) == 0) { // Computer chooses randomly or strategically.
             compChoice = strategicChoice(prevChoice, wonPrev);
         } else {
             compChoice = randomChoice();
@@ -36,16 +41,34 @@ public class RPS {
             tie = true;
         }
 
-    }
-
-    public static void runGame() {
-        playRound(1, true);
+        System.out.println("Computer chose " + key.get(compChoice) + ", You played " + key.get(userChoice));
         if (userWin && !tie) {
-            System.out.println
+            System.out.println("Congratulations! You Won!");
+        } else if (tie) {
+            System.out.println("You tied.");
+        } else if (!userWin && !tie) {
+            System.out.println("Womp, Womp. You lost. D:");
         }
     }
-    public static void main(String[] args){
-        playRound(1, true);
+
+
+
+
+    public static void main(String[] args) {
+        key.put(1, "Rock");
+        key.put(2, "Paper");
+        key.put(3, "Scissors");
+
+        while (true) {
+            playRound(1, true);
+            System.out.print("Play again? (y/n): ");
+            String again = scan.next();
+            if (!again.equalsIgnoreCase("y")) {
+                System.out.println("Thanks for playing!");
+                break;
+            }
+        }
+        scan.close();
     }
 }
 
