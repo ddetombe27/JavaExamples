697da0fc2bab7a3bee26654766558a8572642c06
David deTombe <david.detombe@gmail.com>
2025-10-07 13:19:29 +0200

    Finished Unit 1 Activities

--- src/Receipt4.java
+++ src/Receipt4.java
@@ -5,21 +5,41 @@
  * @author – Class period
  */
 
-public class Receipt3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        String school = "Short Name";
-        String whitespace = "                     *".substring(school.length());
+public class Receipt4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What school do you go to? (4 words)");
+        String school = scan.nextLine();
+        String schoolLast3 = school.substring(school.indexOf(" ") + 1);
+        String schoolLast2 = schoolLast3.substring(schoolLast3.indexOf(" ") + 1);
+        String schoolLast = schoolLast2.substring(schoolLast2.indexOf(" ") + 1);
+        String schoolAcronym = school.substring(0, 1) + schoolLast3.substring(0, 1) + schoolLast2.substring(0, 1) + schoolLast.substring(0, 1);
+
+        String whitespace = "                     *";
+        String schoolWhitespace = whitespace.substring(schoolAcronym.length());
 
         double costDrink = 1.50;
         double costCandy = 1.25;
         double costHotDog = 2.75;
-        double costHamburger = 3.50;
+        double costHamburger = 3.30;
         int orderNumber = (int) (Math.random() * 100) + 1;
-        int numDrink = (int) (Math.random() * 3);
-        int numCandy = (int) (Math.random() * 3);
-        int numHotDog = (int) (Math.random() * 3);
-        int numHamburger = (int) (Math.random() * 3);
+
+        System.out.println("Please buy no more than three of an item.");
+
+        System.out.println("How many drinks would you like?");
+        int numDrink = scan.nextInt();
+        System.out.println("How many candies would you like?");
+        int numCandy = scan.nextInt();
+        System.out.println("How many hotdogs would you like?");
+        int numHotDog = scan.nextInt();
+        System.out.println("How many hamburgers would you like?");
+        int numHamburger = scan.nextInt();
+
+        scan.close();
+
         double subtotal = numDrink * costDrink + numCandy * costCandy + numHotDog * costHotDog + numHamburger * costHamburger;
         double taxRate = 0.13;
         double totalTax = subtotal * taxRate;
@@ -27,7 +47,7 @@ public class Receipt3 {
 
         System.out.println("**************************************");
         System.out.println("*                                    *");
-        System.out.println("*     " + school + " Snack Bar" + whitespace);
+        System.out.println("*     " + schoolAcronym + " Snack Bar" + schoolWhitespace);
         System.out.println("*                                    *");
         System.out.println("*     Drink ..........." + String.format("%.2f", costDrink) + "€         *");
         System.out.println("*     Candy ..........." + String.format("%.2f", costCandy) + "€         *");
