5b49e6409c56a15a8959915b51fb4cb3bfaa7c96
David deTombe <david.detombe@gmail.com>
2025-10-13 15:50:39 +0200

    Finished logic of Tic Tac Toe

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,81 @@
-import java.util.Scanner;
-
-public class Main
-{
-    private static Scanner scan = new Scanner(System.in);
-
-    public static void main(String[] args)
-    {
-        // TODO #1. Change the adventure text below, especially the ?'s. 
-        // Be creative!
-        System.out.println("You are on an island surrounded by water.");
-        System.out.println("There is a path to the forest to the north, "
-                + "the sea to the south.");
-        System.out.println("Which way do you want to go (n,s)?");
-        String command = scan.next(); // use nextLine() in your own IDE
-        if (command.equals("n"))
-        {
-            System.out.println("You go north.");
-            forest();
-        } else if (command.equals("s")) {
-            System.out.println("You walk south");
-            cave();
-        }
-        // TODO #2: Add else-ifs for command equals s, e, or w,
-        // calling a method you will write below for each location.
-        // Add an else error message "You can't go in that direction!" 
-        // Change the input below the coding area to test these.
-
-
-
-
-        System.out.println("End of adventure!");
-        scan.close();
-    }
-
-    // TODO #3: Complete this method
-    // north from main goes to the forest
-    public static void forest()
-    {
-        System.out.println("You enter a dark forest and see ?");
-        System.out.println("Do you want to walk e or w?");
-
-        // Add more if/else-if statements for the next action
-        //  and call your other methods to move to other locations
-        String command = scan.next(); // use nextLine() in your own IDE
-        if (command.equals("e"))
-        {
-            System.out.println("You move east and reach the sea");
-            sea();
-        } else if (command.equals("w")) {
-            System.out.println("You walk west");
-            cave();
-        }
-    }
-
-    // TODO #4: Complete this method
-    // south from main or east from forest goes to the sea
-    // (feel free to rename to another location of your choice)
-    public static void sea()
-    {
-        System.out.println("You see the beach before you");
-        System.out.println("Do you want to walk n or e or s");
-        String command = scan.next(); // use nextLine() in your own IDE
-        if (command.equals("e"))
-        {
-            System.out.println("You drown");
-        } else if (command.equals("n")) {
-            System.out.println("You trip over some driftwood and die");
-        } else if (command.equals("s")) {
-            System.out.println("You walk into a crab, which pinches you to death");
-        }
-
-    }
-
-    public static void cave() {
-        System.out.println("You fall into a cave and die");
-    }
-
-    // TODO #5:  Add at least 2 more static methods for 2 more locations
-
-}
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//    private static Scanner scan = new Scanner(System.in);
+//
+//    public static void main(String[] args)
+//    {
+//        // TODO #1. Change the adventure text below, especially the ?'s.
+//        // Be creative!
+//        System.out.println("You are on an island surrounded by water.");
+//        System.out.println("There is a path to the forest to the north, "
+//                + "the sea to the south.");
+//        System.out.println("Which way do you want to go (n,s)?");
+//        String command = scan.next(); // use nextLine() in your own IDE
+//        if (command.equals("n"))
+//        {
+//            System.out.println("You go north.");
+//            forest();
+//        } else if (command.equals("s")) {
+//            System.out.println("You walk south");
+//            cave();
+//        }
+//        // TODO #2: Add else-ifs for command equals s, e, or w,
+//        // calling a method you will write below for each location.
+//        // Add an else error message "You can't go in that direction!"
+//        // Change the input below the coding area to test these.
+//
+//
+//
+//
+//        System.out.println("End of adventure!");
+//        scan.close();
+//    }
+//
+//    // TODO #3: Complete this method
+//    // north from main goes to the forest
+//    public static void forest()
+//    {
+//        System.out.println("You enter a dark forest and see ?");
+//        System.out.println("Do you want to walk e or w?");
+//
+//        // Add more if/else-if statements for the next action
+//        //  and call your other methods to move to other locations
+//        String command = scan.next(); // use nextLine() in your own IDE
+//        if (command.equals("e"))
+//        {
+//            System.out.println("You move east and reach the sea");
+//            sea();
+//        } else if (command.equals("w")) {
+//            System.out.println("You walk west");
+//            cave();
+//        }
+//    }
+//
+//    // TODO #4: Complete this method
+//    // south from main or east from forest goes to the sea
+//    // (feel free to rename to another location of your choice)
+//    public static void sea()
+//    {
+//        System.out.println("You see the beach before you");
+//        System.out.println("Do you want to walk n or e or s");
+//        String command = scan.next(); // use nextLine() in your own IDE
+//        if (command.equals("e"))
+//        {
+//            System.out.println("You drown");
+//        } else if (command.equals("n")) {
+//            System.out.println("You trip over some driftwood and die");
+//        } else if (command.equals("s")) {
+//            System.out.println("You walk into a crab, which pinches you to death");
+//        }
+//
+//    }
+//
+//    public static void cave() {
+//        System.out.println("You fall into a cave and die");
+//    }
+//
+//    // TODO #5:  Add at least 2 more static methods for 2 more locations
+//
+//}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,55 +1,154 @@
-//import java.util.*;
-//
-//public class TicTacToe {
-//    public static HashSet<Integer> p1Squares = new HashSet<Integer>();
-//    public static HashSet<Integer> p2Squares = new HashSet<Integer>();
-//    public static HashSet<Integer> filledSquares = new HashSet<Integer>();
-//
-//    public static boolean checkWin() {
-//        return false;
-//    }
-//
-//    public static void reset() {
-//        p1Squares.clear();
-//        p2Squares.clear();
-//        emptySquares.clear();
-//        filledSquares.clear();
-//        Collections.addAll(emptySquares, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-//    }
-//
-//    public static String getString(int square) {
-//        if (p1Squares.contains(square)) {
-//            return "X";
-//        }
-//        if (p2Squares.contains(square)) {
-//            return "O";
-//        }
-//        return " ";
-//
-//    }
-//
-//    public static void playSquare(int square, int player) {
-//            filledSquares.add(square);
-//            if (player == 1) {
-//                p1Squares.add(square);
-//                return;
-//            } else if (player == 2) {
-//                p2Squares.add(square);
-//                return;
-//            }
-//
-//            System.out.println("Something went wrong...");
-//
-//        }
-//    }
-//
-//    public static void printSquare() {
-//
-//    }
-//
-//    public static void main(String[] args){
-//        System.out.println("Testing...");
-//        reset();
-//        //playSquare();
-//    }
-//}
+import java.util.*;
+
+public class TicTacToe {
+    public static HashSet<Integer> p1Squares = new HashSet<Integer>();
+    public static HashSet<Integer> p2Squares = new HashSet<Integer>();
+    public static HashSet<Integer> filledSquares = new HashSet<Integer>();
+    public static HashSet<Integer> win1 = new HashSet<Integer>();
+    public static HashSet<Integer> win2 = new HashSet<Integer>();
+    public static HashSet<Integer> win3 = new HashSet<Integer>();
+    public static HashSet<Integer> win4 = new HashSet<Integer>();
+    public static HashSet<Integer> win5 = new HashSet<Integer>();
+    public static HashSet<Integer> win6 = new HashSet<Integer>();
+    public static HashSet<Integer> win7 = new HashSet<Integer>();
+    public static HashSet<Integer> win8 = new HashSet<Integer>();
+    public static HashMap<String, HashSet<Integer>> winConditions = new HashMap<>();
+    public static int winner = 0;
+
+    public static boolean checkWin() {
+        return false;
+    }
+
+    public static void reset() {
+        p1Squares.clear();
+        p2Squares.clear();
+        filledSquares.clear();
+
+        win1.clear();
+        win2.clear();
+        win3.clear();
+        win4.clear();
+        win5.clear();
+        win6.clear();
+        win7.clear();
+        win8.clear();
+        winConditions.clear();
+
+        win1.add(1);
+        win1.add(2);
+        win1.add(3);
+
+        win2.add(4);
+        win2.add(5);
+        win2.add(6);
+
+        win3.add(7);
+        win3.add(8);
+        win3.add(9);
+
+        win4.add(1);
+        win4.add(4);
+        win4.add(7);
+
+        win5.add(2);
+        win5.add(5);
+        win5.add(8);
+
+        win6.add(3);
+        win6.add(6);
+        win6.add(9);
+
+        win7.add(1);
+        win7.add(5);
+        win7.add(9);
+
+        win8.add(3);
+        win8.add(5);
+        win8.add(7);
+
+        winConditions.put("1", win1);
+        winConditions.put("2", win2);
+        winConditions.put("3", win3);
+        winConditions.put("4", win4);
+        winConditions.put("5", win5);
+        winConditions.put("6", win6);
+        winConditions.put("7", win7);
+        winConditions.put("8", win8);
+    }
+
+    public static String getSquare(int square) {
+        if (p1Squares.contains(square)) {
+            return "X";
+        }
+        if (p2Squares.contains(square)) {
+            return "O";
+        }
+        return String.valueOf(square);
+
+    }
+
+    public static void playSquare(int square, int player) {
+        filledSquares.add(square);
+        if (player == 1) {
+            p1Squares.add(square);
+            return;
+        } else if (player == 2) {
+            p2Squares.add(square);
+            return;
+        }
+
+        System.out.println("Something went wrong...");
+
+    }
+
+    public static int winDetect() {
+        for (int i = 1; i <= 8; i++) {  // only 8 win conditions
+            HashSet<Integer> winSet = winConditions.get(String.valueOf(i));
+            if (p1Squares.containsAll(winSet)) {
+                return 1;
+            } else if (p2Squares.containsAll(winSet)) {
+                return 2;
+            }
+        }
+        return 0;
+    }
+
+
+    public static void printBoard() {
+        System.out.println(getSquare(1) + " | " + getSquare(2) + " | " + getSquare(3));
+        System.out.println("—————————");
+        System.out.println(getSquare(4) + " | " + getSquare(5) + " | " + getSquare(6));
+        System.out.println("—————————");
+        System.out.println(getSquare(7) + " | " + getSquare(8) + " | " + getSquare(9));
+    }
+
+    public static void gameplay() {
+        Scanner scan = new Scanner(System.in);
+        int play;
+        int currentPlayer;
+        reset();
+        System.out.println("Welcome to Tic-Tac-Toe!\n");
+        System.out.println("These are the positions on the board:");
+        printBoard();
+
+        for (int loop = 0; loop < 9; loop++) {
+            currentPlayer = (loop % 2) + 1;
+            System.out.println("Player " + currentPlayer + "'s turn: ");
+            play = scan.nextInt();
+            playSquare(play, currentPlayer);
+            printBoard();
+            winner = winDetect();
+            if (winDetect() > 0) {
+                break;
+            }
+        }
+
+        System.out.println("Player " + winner + " wins!");
+    }
+
+
+    public static void main(String[] args) {
+        gameplay();
+    }
+}
+
